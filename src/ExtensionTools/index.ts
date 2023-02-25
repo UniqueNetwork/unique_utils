a2195f56c25715b8439dea39e61030671f8707df
Alex Saft <fend25@gmail.com>
2023-02-26 01:03:15 +0300

    fix: fixed type exports

--- src/ExtensionTools/index.ts
+++ src/ExtensionTools/index.ts
@@ -14,12 +14,21 @@ export default ExtensionTools
 // types
 // =========================================
 
+
+
 export type {
   AddEthereumChainParameter,
   UNIQUE_CHAIN,
   UpdateReason,
   IEthereumAccountResult,
+  IEthereumExtensionError,
 } from './ethereum'
+
+export type IGetAccountsUnsafe = typeof Ethereum.getAccountsUnsafe
+export type IRequestAccountsUnsafe = typeof Ethereum.requestAccountsUnsafe
+
+export type IEnableAndLoadAllWalletsUnsafe = typeof Polkadot.enableAndLoadAllWalletsUnsafe
+export type ILoadEnabledWalletsUnsafe = typeof Polkadot.loadEnabledWalletsUnsafe
 
 export type {
   IPolkadotExtensionWallet,
@@ -29,6 +38,7 @@ export type {
   IPolkadotExtensionLoadWalletsResult,
   IPolkadotExtensionListWalletsResult,
   IPolkadotExtensionLoadWalletByNameResult,
+  IPolkadotExtensionLoadWalletsError,
   SignerPayloadJSON,
   SignerPayloadRaw,
   SignerPayloadRawWithAddressAndTypeOptional,
